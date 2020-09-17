ef666023a9cb5abda16c6711b3779dc2f94595b1
vauxhall <zero.griffin@gmail.com>
2020-09-17 14:31:29 +0200

    Update project

---
+++ The_Forest_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Forest_BookApp.swift
+//  The Forest Book
+//
+//  Created by vaux on 17.09.20.
+//
+
+import SwiftUI
+
+@main
+struct The_Forest_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
